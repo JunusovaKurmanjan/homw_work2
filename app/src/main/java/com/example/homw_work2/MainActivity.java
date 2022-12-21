ecea9da585b7080622582ad49e53ac38fd0e569c
JunusovaKurmanjan <kurmanzanzunusova@gmail.com>
2022-12-21 20:21:46 +0300

    Initial commit

---
+++ app/src/main/java/com/example/homw_work2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.homw_work2;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().add(R.id.container, new FirstFragment1()).commit();
+        }
+    }
+}
